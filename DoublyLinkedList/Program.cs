93475cd96c7c80d41aee9076c44a15678aef8f80
Plotva52 <425680@mail.ru>
2021-05-16 15:57:34 +0300

    Добавьте файлы проекта.

---
+++ DoublyLinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new DataStructures.DoublyLinkedList();
+          //  var t = list.AddFirst("2");
+            list.AddFirst("1");
+            list.AddLast("2");
+           var t= list.AddFirst("0");
+            list.AddLast("3");
+            list.AddLast("3");
+            list.RemoveElement(t);
+            list.GetAndRemoveLast();
+            Console.WriteLine(list.First.Value);
+            var elem = list.First.NextElement;
+            while(elem != null)
+            {
+                Console.WriteLine(elem.Value);
+                elem = elem.NextElement;
+            }            
+        }
+    }
+}
---
+++ DoublyLinkedListTests/UnitTest1.cs
@@ -0,0 +1,169 @@
+using System;
+using Xunit;
+
+namespace DoublyLinkedListTests
+{
+    public class UnitTest1
+    {
+        private DataStructures.DoublyLinkedList list = new DataStructures.DoublyLinkedList();
+
+        [Fact]
+        public void AddFirstTest()
+        {
+            list.AddFirst("1");
+            list.AddFirst("2");
+            list.AddFirst("3");
+            list.AddFirst("4");
+            string[] expectedSubArray = new string[4] {"4", "3", "2", "1" };
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void AddLastTest()
+        {
+            list.AddLast("1");
+            list.AddLast("2");
+            list.AddLast("3");
+            list.AddLast("4");
+            string[] expectedSubArray = new string[4] { "1", "2", "3", "4" };
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void AddLastFirstTest()
+        {
+            list.AddLast("1");
+            list.AddFirst("2");
+            list.AddLast("3");
+            list.AddFirst("4");
+            string[] expectedSubArray = new string[4] { "4", "2", "1", "3"};
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void AddFirstAddLastTest()
+        {
+            list.AddLast("1");
+            list.AddLast("2");
+            list.AddLast("3");
+            list.AddLast("4");
+            list.AddFirst("1");
+            list.AddFirst("2");
+            list.AddFirst("3");
+            list.AddFirst("4");
+            string[] expectedSubArray = new string[8] { "4", "3", "2", "1", "1", "2", "3", "4" };
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+
+
+        [Fact]
+        public void RemoveAllLastTest()
+        {
+            list.AddLast("1");
+            list.AddFirst("2");
+            list.AddLast("3");
+            list.AddFirst("4");
+            list.GetAndRemoveLast();
+            list.GetAndRemoveLast();
+            list.GetAndRemoveLast();
+            list.GetAndRemoveLast();
+            list.GetAndRemoveLast();
+            list.GetAndRemoveLast();
+            string[] expectedSubArray = new string[0];
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void RemoveAllFirstTest()
+        {
+            list.AddLast("1");
+            list.AddFirst("2");
+            list.AddLast("3");
+            list.AddFirst("4");
+            list.GetAndRemoveFirst();
+            list.GetAndRemoveFirst();
+            list.GetAndRemoveFirst();
+            list.GetAndRemoveFirst();
+            list.GetAndRemoveFirst();
+            list.GetAndRemoveFirst();
+            string[] expectedSubArray = new string[0];
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void AddAndRemoveAllTest()
+        {
+            list.RemoveElement(list.AddLast("1"));
+            list.RemoveElement(list.AddFirst("2"));
+            list.RemoveElement(list.AddLast("3"));
+            list.RemoveElement(list.AddFirst("4"));
+            string[] expectedSubArray = new string[0];
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void AllRemoveElementTest()
+        {
+            list.AddLast("1");
+            list.AddLast("2");
+            list.AddLast("3");
+            list.AddLast("4");
+            list.AddFirst("1");
+            list.AddFirst("2");
+            list.AddFirst("3");
+            list.AddFirst("4");
+
+            var elem = list.First;
+
+            while (elem != null)
+            {
+                list.RemoveElement(elem);
+                elem = elem.NextElement;
+            }
+
+            string[] expectedSubArray = new string[0];
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void InsertAfterTest()
+        {
+            list.AddLast("1");
+            var elem = list.AddLast("2");
+            list.AddLast("4");
+            list.InsertAfter(elem, "3");
+
+            string[] expectedSubArray = new string[4] { "1", "2", "3", "4" };
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        [Fact]
+        public void InsertAfterFirstAndLastTest()
+        {
+            list.AddLast("1");
+            list.AddLast("4");
+            list.InsertAfter(list.Last, "5");
+            list.InsertAfter(list.First, "3");
+            list.InsertAfter(list.First, "2");
+
+            string[] expectedSubArray = new string[5] { "1", "2", "3", "4", "5" };
+            Assert.Equal(expectedSubArray, GetValuesFromList());
+        }
+
+        private string[] GetValuesFromList()
+        {
+            string[] actualArray = new string[list.Count];
+
+            var elem = list.First;
+            var i = 0;
+            while(elem != null)
+            {
+                actualArray[i] = elem.Value;
+                i++;
+                elem = elem.NextElement;
+            }
+            return actualArray;
+        }
+    }
+}
